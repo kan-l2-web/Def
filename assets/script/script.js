1185c8bbf138cd6829ed31ae37f090f1f7b01b20
kan-l2-web <kandemirkarasal8@gmail.com>
2022-04-29 21:15:43 +0700

    29.04.22

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,20 @@
+document.body.onload = function () {
+
+	setTimeout(function() {
+		const preloader = document.querySelector('.preloader');
+
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done')
+		}
+	}, 1000);
+
+}
+
+
 const 
 	 headerNav = document.querySelector('.header__nav');
 	 headerBurger = document.querySelector('.header__burger');
+
 
 headerBurger.addEventListener('click', function(e) {
 	document.body.classList.toggle('lock');
